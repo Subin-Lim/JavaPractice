24c58df06aaee2364d87ddfbe1f05897be2cceb8
Addictive <163709843+Subin-Lim@users.noreply.github.com>
2024-09-16 23:18:54 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 13936 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/"); // 이스케이프 문자 사용
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
