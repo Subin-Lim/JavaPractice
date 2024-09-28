7a526d59d587dbc5868d77c51994335171942be8
Addictive <163709843+Subin-Lim@users.noreply.github.com>
2024-09-28 21:58:20 +0900

    [level 0] Title: 나이 출력, Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int age) {
+        int nai = 2022-age+1;
+        
+        return nai;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int agee = 40;
+        System.out.println(sol.solution(agee));
+    }
+}
