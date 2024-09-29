499edf32675cdac7aaa837e6e019314ac483ea9b
Addictive <163709843+Subin-Lim@users.noreply.github.com>
2024-09-29 23:31:18 +0900

    [level 0] Title: 두 수의 차, Time: 0.03 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int result = num1-num2;
+        
+        return result;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int numo = 2;
+        int numt = 3;
+        System.out.println(sol.solution(numo, numt));
+    }
+}
